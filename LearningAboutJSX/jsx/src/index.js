68623af07636d2939c785eb86d25354641d4da47
Scott Ziegler <47644964+scottiez12@users.noreply.github.com>
2021-05-20 16:47:06 -0400

    Sync
    Created next project

--- LearningAboutJSX/jsx/src/index.js
+++ LearningAboutJSX/jsx/src/index.js
@@ -2,16 +2,28 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
+//adding this will fire the hot reload if it's not working by default for whatever reason..
+if (module.hot) {
+  module.hot.accept();
+}
+
+// function getButtonText() {
+//   return "Click on me!";
+// }
+
 //create a react component
 const App = () => {
+  const buttonText = "Click Me!@!@!@";
+
   return (
     <div>
-      <label className="label" for="name">
+      <label className="label" htmlFor="name">
         Enter Name:
       </label>
       <input id="name" type="text" />
       <button style={{ color: "white", backgroundColor: "blue" }}>
-        Submit
+        {buttonText}
+        {/* {getButtonText()} */}
       </button>
     </div>
   );
