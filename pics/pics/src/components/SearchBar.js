12423c79e69d2df62ca6efe285e1bc3b5f4d813e
Scott Ziegler <47644964+scottiez12@users.noreply.github.com>
2021-05-26 16:49:35 -0400

    Sync Finish section 7

--- pics/pics/src/components/SearchBar.js
+++ pics/pics/src/components/SearchBar.js
@@ -1,17 +1,24 @@
 import React from "react";
 
 class SearchBar extends React.Component {
+  state = { term: "" };
+
   //callback function
   //   onInputChange(event) {
   //     console.log(event.target.value);
   //   }
 
-  state = { term: "" };
+  //one way to fix the 'this' issue.. arrow functions auto-bind the functions 'this' to the 'this' representing the class
+  onFormSubmit = (event) => {
+    event.preventDefault();
+    this.props.onSubmit(this.state.term);
+    //console.log(this.state.term);
+  };
 
   render() {
     return (
       <div className="ui segment">
-        <form className="ui form">
+        <form className="ui form" onSubmit={this.onFormSubmit}>
           <div className="field">
             <label>Image Search</label>
             {/* if you put () on a callback function like onChange, it will run every time render runs.. so we don't want to do that */}
@@ -25,7 +32,7 @@ class SearchBar extends React.Component {
             {/* this below refactoring is making this a controlled element... */}
             <input
               type="text"
-              calue={this.state.term}
+              value={this.state.term}
               onChange={(e) => this.setState({ term: e.target.value })}
             ></input>
           </div>
