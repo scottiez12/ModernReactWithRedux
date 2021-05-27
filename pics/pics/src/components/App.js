d5a4c8ab647163ed029bffa0dce7e7a40e1a2e11
Scott Ziegler <47644964+scottiez12@users.noreply.github.com>
2021-05-27 16:58:18 -0400

    Using APIs in react

--- pics/pics/src/components/App.js
+++ pics/pics/src/components/App.js
@@ -1,16 +1,36 @@
 import React from "react";
+import axios from "axios";
 import SearchBar from "./SearchBar";
 
 class App extends React.Component {
+  state = { images: [] };
+
   //define callback function to pass down as a prop
-  onSearchSubmit(term) {
-    console.log(term);
-  }
+  onSearchSubmit = async (term) => {
+    //console.log(term);
+    //this is where we want to call the api thru ajax with axios...
+    const response = await axios.get("https://api.unsplash.com/search/photos", {
+      params: {
+        query: term,
+      },
+      headers: {
+        Authorization: "Client-ID EdqjX_wmTnKa73-od-GfAVwCJI9zz6sBNGqFnjCUasA",
+      },
+    });
+    //one way to do the callback
+    // .then((response) => {
+    //   console.log(response.data.results);
+    // });
+
+    //console.log(response.data.results);
+    this.setState({ images: response.data.results });
+  };
 
   render() {
     return (
       <div className="ui container" style={{ marginTop: "10px" }}>
         <SearchBar onSubmit={this.onSearchSubmit} />
+        Found: {this.state.images.length} images.
       </div>
     );
   }
