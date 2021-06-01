5a2137fa023a563a189807bd30ac5fb85bf42ebb
Scott Ziegler <47644964+scottiez12@users.noreply.github.com>
2021-06-01 15:54:47 -0400

    useRef and end of the dropdown widget

--- Hooks/widgets/src/App.js
+++ Hooks/widgets/src/App.js
@@ -1,6 +1,7 @@
-import React from "react";
+import React, { useState } from "react";
 import Accordion from "./components/Accordion";
 import Search from "./components/Search";
+import Dropdown from "./components/Dropdown";
 
 const items = [
   {
@@ -17,11 +18,32 @@ const items = [
   },
 ];
 
+const options = [
+  { label: "The Color Red", value: "red" },
+  {
+    label: "The color GREEN",
+    value: "green",
+  },
+  { label: "A shade of blue", value: "blue" },
+];
+
 export default () => {
+  const [selected, setSelected] = useState(options[0]);
+  const [showDropdown, setShowDropdown] = useState(true);
   return (
     <div>
       {/* <Accordion items={items} /> */}
-      <Search />
+      {/* <Search /> */}
+      <button onClick={() => setShowDropdown(!showDropdown)}>
+        Toggle Dropdown
+      </button>
+      {showDropdown ? (
+        <Dropdown
+          options={options}
+          onSelectedChange={setSelected}
+          selected={selected}
+        />
+      ) : null}
     </div>
   );
 };
