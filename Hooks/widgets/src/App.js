649d70e67e88813c1dd0d6e501f4b3fa664d6e75
Scott Ziegler <47644964+scottiez12@users.noreply.github.com>
2021-06-01 16:40:17 -0400

    Sync; almost done with the hooks section

--- Hooks/widgets/src/App.js
+++ Hooks/widgets/src/App.js
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import Accordion from "./components/Accordion";
 import Search from "./components/Search";
 import Dropdown from "./components/Dropdown";
+import Translate from "./components/Translate";
 
 const items = [
   {
@@ -28,13 +29,13 @@ const options = [
 ];
 
 export default () => {
-  const [selected, setSelected] = useState(options[0]);
-  const [showDropdown, setShowDropdown] = useState(true);
+  // const [selected, setSelected] = useState(options[0]);
+  // const [showDropdown, setShowDropdown] = useState(true);
   return (
     <div>
       {/* <Accordion items={items} /> */}
       {/* <Search /> */}
-      <button onClick={() => setShowDropdown(!showDropdown)}>
+      {/* <button onClick={() => setShowDropdown(!showDropdown)}>
         Toggle Dropdown
       </button>
       {showDropdown ? (
@@ -43,7 +44,8 @@ export default () => {
           onSelectedChange={setSelected}
           selected={selected}
         />
-      ) : null}
+      ) : null} */}
+      <Translate />
     </div>
   );
 };
---
+++ Hooks/widgets/src/components/Translate.js
@@ -0,0 +1,37 @@
+import React, { useState } from "react";
+import Dropdown from "./Dropdown";
+import Convert from "./Convert";
+
+//google translate api key via udemy.. only works on localhost:3000
+//  AIzaSyCHUCmpR7cT_yDFHC98CZJy2LTms-IwDlM
+
+const options = [
+  { label: "Afrikaans", value: "af" },
+  { label: "Arabic", value: "ar" },
+  { label: "Hindi", value: "hi" },
+  { label: "Japanese", value: "ja" },
+];
+
+const Translate = () => {
+  const [language, setLanguage] = useState(options[0]);
+  const [text, setText] = useState("");
+  return (
+    <div className="ui form container">
+      <div className="field">
+        <label>Enter Text</label>
+        <input value={text} onChange={(e) => setText(e.target.value)} />
+      </div>
+      <Dropdown
+        label="Select a Language"
+        selected={language}
+        onSelectedChange={setLanguage}
+        options={options}
+      />
+      <hr />
+      <h3 className="ui header">Output</h3>
+      <Convert text={text} language={language} />
+    </div>
+  );
+};
+
+export default Translate;
