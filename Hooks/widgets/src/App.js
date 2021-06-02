02a22821df1e9a642aed0c2207f2daa06c70b19a
Scott Ziegler <47644964+scottiez12@users.noreply.github.com>
2021-06-02 12:05:53 -0400

    Finished nav without React Router

--- Hooks/widgets/src/App.js
+++ Hooks/widgets/src/App.js
@@ -3,6 +3,8 @@ import Accordion from "./components/Accordion";
 import Search from "./components/Search";
 import Dropdown from "./components/Dropdown";
 import Translate from "./components/Translate";
+import Route from "./components/Route";
+import Header from "./components/Header";
 
 const items = [
   {
@@ -28,24 +30,31 @@ const options = [
   { label: "A shade of blue", value: "blue" },
 ];
 
+// eslint-disable-next-line import/no-anonymous-default-export
 export default () => {
+  const [selected, setSelected] = useState(options[0]);
   // const [selected, setSelected] = useState(options[0]);
   // const [showDropdown, setShowDropdown] = useState(true);
   return (
     <div>
-      {/* <Accordion items={items} /> */}
-      {/* <Search /> */}
-      {/* <button onClick={() => setShowDropdown(!showDropdown)}>
-        Toggle Dropdown
-      </button>
-      {showDropdown ? (
+      <Header />
+      <Route path="/">
+        <Accordion items={items} />
+      </Route>
+      <Route path="/list">
+        <Search />
+      </Route>
+      <Route path="/translate">
+        <Translate />
+      </Route>
+      <Route path="/dropdown">
         <Dropdown
+          label="Select a Color"
           options={options}
-          onSelectedChange={setSelected}
           selected={selected}
+          onSelectedChange={setSelected}
         />
-      ) : null} */}
-      <Translate />
+      </Route>
     </div>
   );
 };
