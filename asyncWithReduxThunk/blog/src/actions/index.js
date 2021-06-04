bf9192116fe8d6c782de93c3f90b4c7d8e99db4e
Scott Ziegler <47644964+scottiez12@users.noreply.github.com>
2021-06-04 14:48:27 -0400

    Sync

--- asyncWithReduxThunk/blog/src/actions/index.js
+++ asyncWithReduxThunk/blog/src/actions/index.js
@@ -7,7 +7,13 @@ export const fetchPosts = () => async (dispatch) => {
   dispatch({ type: "FETCH_POSTS", payload: response.data });
 };
 
-//so this is totally fine not having to us async
+export const fetchUser = (id) => async (dispatch) => {
+  const response = await jsonplaceholder.get(`/users/${id}`);
+
+  dispatch({ type: "FETCH_USER", payload: response.data });
+};
+
+//so this is totally fine if we dont have to do anything async...
 // export const selectPost = () => {
 //   return {
 //     type: "SELECT_POST",
