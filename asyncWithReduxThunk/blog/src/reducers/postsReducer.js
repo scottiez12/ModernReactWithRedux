7e89346792635bf6d3699addcacd806ef6ad3ba2
Scott Ziegler <47644964+scottiez12@users.noreply.github.com>
2021-06-04 11:31:32 -0400

    Working sync section 19

--- asyncWithReduxThunk/blog/src/reducers/postsReducer.js
+++ asyncWithReduxThunk/blog/src/reducers/postsReducer.js
@@ -1,3 +1,9 @@
-export default () => {
-  return 123;
+// eslint-disable-next-line import/no-anonymous-default-export
+export default (state = [], action) => {
+  switch (action.type) {
+    case "FETCH_POSTS":
+      return action.payload;
+    default:
+      return state;
+  }
 };
