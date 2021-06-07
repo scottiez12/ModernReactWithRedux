49d50918484033395fcea4c6104c4cc7dd42f3e0
Scott Ziegler <47644964+scottiez12@users.noreply.github.com>
2021-06-07 10:02:35 -0400

    End of Section 19 sync

--- asyncWithReduxThunk/blog/src/actions/index.js
+++ asyncWithReduxThunk/blog/src/actions/index.js
@@ -1,4 +1,23 @@
 import jsonplaceholder from "../apis/jsonPlaceHolder";
+import _ from "lodash";
+
+export const fetchPostsAndUsers = (id) => async (dispatch, getState) => {
+  await dispatch(fetchPosts());
+  console.log(getState().posts);
+  // const userIds = _.uniq(_.map(getState().posts, "userId"));
+  // console.log(userIds);
+  // userIds.forEach((id) => dispatch(fetchUser(id)));
+
+  //another refactor using lodash
+  //basically each step in the chain receives the result of the argument before it..
+  //so map() automatically receives the results of getState().posts
+  //the .value() at the end SHOULD BE CALLED SOMETHING LIKE .EXECUTE() BECAUSE ITS NEEDED TO TELL THE CHAIN TO ACTUALLY WORK.
+  _.chain(getState().posts)
+    .map("userId")
+    .uniq()
+    .forEach((id) => dispatch(fetchUser(id)))
+    .value();
+};
 
 //correct syntax for async/await with redux thunk, the way it's commonly refactored
 export const fetchPosts = () => async (dispatch) => {
@@ -9,9 +28,17 @@ export const fetchPosts = () => async (dispatch) => {
 
 export const fetchUser = (id) => async (dispatch) => {
   const response = await jsonplaceholder.get(`/users/${id}`);
-
   dispatch({ type: "FETCH_USER", payload: response.data });
 };
+
+// export const fetchUser = (id) => (dispatch) => {
+//   _fetchUser(id, dispatch);
+// };
+
+// const _fetchUser = _.memoize(async (id, dispatch) => {
+//   const response = await jsonplaceholder.get(`/users/${id}`);
+//   dispatch({ type: "FETCH_USER", payload: response.data });
+// });
 
 //so this is totally fine if we dont have to do anything async...
 // export const selectPost = () => {
