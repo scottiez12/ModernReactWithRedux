fc339375cb2bb81ff1078401691a12400a4683bb
Scott Ziegler <47644964+scottiez12@users.noreply.github.com>
2021-06-21 16:51:42 -0400

    End of the basic CRUD stuff and more
    Skipping the streaming stuff, moving onto section 27 context

--- streams/client/src/components/Modal.js
+++ streams/client/src/components/Modal.js
@@ -1,22 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import history from "../history";
 
 const Modal = (props) => {
   return ReactDOM.createPortal(
-    <div
-      className="ui dimmer modals visible active"
-      onClick={() => history.push("/")}
-    >
-      <div className="ui standard modal visible active">
-        <div className="header">Delete Stream</div>
-        <div className="content">
-          <p>Are you sure you want to delete this stream?</p>
-        </div>
-        <div className="actions">
-          <button className="ui primary button">Delete</button>
-          <button className="ui button">Cancel</button>
-        </div>
+    <div className="ui dimmer modals visible active" onClick={props.onDismiss}>
+      <div
+        className="ui standard modal visible active"
+        onClick={(e) => e.stopPropagation()}
+      >
+        <div className="header">{props.title}</div>
+        <div className="content">{props.content}</div>
+        <div className="actions">{props.actions}</div>
       </div>
     </div>,
     document.querySelector("#modal")
